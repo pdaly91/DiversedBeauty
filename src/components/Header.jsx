ce1353ed32a77792c19f1f5addd25009b60d7206
pdaly91 <patrickdaly91@gmail.com>
2024-01-05 13:39:04 -0800

    small style adjustments

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,13 +13,13 @@ const Header = () => {
         </div>
         {/* Mobile Version */}
         <div className="lg:hidden flex-1 flex justify-end">
-          <button className="cursor-pointer duration-300 hover:scale-110 my-2 mx-2" onClick={() => setMenuToggle(!menuToggle)}>
+          <button className="cursor-pointer duration-300 hover:scale-110 my-2 mr-10" onClick={() => setMenuToggle(!menuToggle)}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
             </svg>
           </button>
           <div className={`fixed z-40 left-0 top-20 right-0 w-full bg-gray-100 overflow-hidden flex flex-col lg:hidden origin-top duration-700 ${!menuToggle ? "h-0" : "h-full"}`}>
-            <div className="flex flex-col content-center w-full gap-4">
+            <div className="flex flex-col content-center w-full gap-4 font-light">
               <div className="m-auto">Home</div>
               <div className="m-auto">Products</div>
               <div className="m-auto">About</div>
@@ -29,7 +29,7 @@ const Header = () => {
         </div>
         {/* Desktop Version */}
         <div className="hidden lg:flex flex-1 justify-end">
-          <div className="flex flex-row gap-4 w-1/2 justify-evenly mr-10">
+          <div className="flex flex-row gap-4 w-1/2 justify-evenly mr-10 font-light">
             <div className="m-auto">Home</div>
             <div className="m-auto">Products</div>
             <div className="m-auto">About</div>
