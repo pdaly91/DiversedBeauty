f90a0b12e5c678f2c69913d5f4395f507f37cd91
pdaly91 <patrickdaly91@gmail.com>
2024-01-05 13:32:20 -0800

    move nav to right and add desktop version

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,16 +4,22 @@ const Header = () => {
   const [menuToggle, setMenuToggle] = useState(false);
   return (
     <>
-      <div className="flex flex-row m-auto h-20 items-center justify-center">
+      <div className="flex flex-row m-auto h-20 items-center justify-center bg-gray-100">
+        {/* empty div to get Logo centered */}
+        <div className="flex-1"></div>
+        {/* Logo */}
+        <div className="m-2">
+          DIVERSED Beauty
+        </div>
         {/* Mobile Version */}
-        <div className="lg:hidden flex-1">
+        <div className="lg:hidden flex-1 flex justify-end">
           <button className="cursor-pointer duration-300 hover:scale-110 my-2 mx-2" onClick={() => setMenuToggle(!menuToggle)}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
             </svg>
           </button>
-          <div className={`fixed z-40 w-full bg-gray-100 overflow-hidden flex flex-col lg:hidden gap-12 origin-top duration-700 ${!menuToggle ? "h-0" : "h-full"}`}>
-            <div className="flex flex-col content-center w-full">
+          <div className={`fixed z-40 left-0 top-20 right-0 w-full bg-gray-100 overflow-hidden flex flex-col lg:hidden origin-top duration-700 ${!menuToggle ? "h-0" : "h-full"}`}>
+            <div className="flex flex-col content-center w-full gap-4">
               <div className="m-auto">Home</div>
               <div className="m-auto">Products</div>
               <div className="m-auto">About</div>
@@ -22,15 +28,14 @@ const Header = () => {
           </div>
         </div>
         {/* Desktop Version */}
-        <div className="hidden lg:flex">
-          Desktop Version
+        <div className="hidden lg:flex flex-1 justify-end">
+          <div className="flex flex-row gap-4 w-1/2 justify-evenly mr-10">
+            <div className="m-auto">Home</div>
+            <div className="m-auto">Products</div>
+            <div className="m-auto">About</div>
+            <div className="m-auto">Contact</div>
+          </div>
         </div>
-        {/* Logo */}
-        <div className="m-2">
-          DIVERSED Beauty
-        </div>
-        {/* empty div to get Logo centered */}
-        <div className="flex-1"></div>
       </div>
     </>
   )
