5bb9f6ca7ef2bb826c4b152455761a7c9d8a08be
pdaly91 <patrickdaly91@gmail.com>
2024-01-05 16:23:56 -0800

    add placeholder About page

---
+++ src/pages/About.jsx
@@ -0,0 +1,16 @@
+import Header from '../components/Header.jsx';
+import Footer from '../components/Footer.jsx';
+
+const About = () => {
+  return (
+    <>
+      <Header />
+      <div>
+        This is the ABOUT Page
+      </div>
+      <Footer />
+    </>
+  )
+};
+
+export default About;
