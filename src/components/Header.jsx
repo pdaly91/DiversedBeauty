301211d7beb3d6b9e1fc639fb5f8df9584003d7a
pdaly91 <patrickdaly91@gmail.com>
2024-01-09 15:02:07 -0800

    change formatting of header to column layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,22 +4,24 @@ const Header = () => {
   const [menuToggle, setMenuToggle] = useState(false);
   return (
     <>
-      <div className="flex flex-row m-auto h-20 items-center justify-center bg-gray-100">
-        {/* empty div to get Logo centered */}
-        <div className="flex-1"></div>
+      <div className="flex flex-col m-auto min-h-20 items-center justify-center bg-gray-100">
+        {/* Established */}
+        <div className="flex-1 mt-12 mb-2 text-xl font-thin">
+          Est. <span className="whitespace-pre text-3xl">  |  </span> 2023
+        </div>
         {/* Logo */}
-        <div className="m-2 text-3xl font-thin">
+        <div className="my-8 text-3xl font-thin">
           DIVERSED Beauty
         </div>
         {/* Mobile Version */}
         <div className="lg:hidden flex-1 flex justify-end">
-          <button className="cursor-pointer duration-300 hover:scale-110 my-2 mr-10" onClick={() => setMenuToggle(!menuToggle)}>
+          <button className="cursor-pointer duration-300 hover:scale-110 mb-8" onClick={() => setMenuToggle(!menuToggle)}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
             </svg>
           </button>
-          <div className={`fixed z-40 left-0 top-20 right-0 w-full bg-gray-100 overflow-hidden flex flex-col lg:hidden origin-top duration-700 ${!menuToggle ? "h-0" : "h-full"}`}>
-            <div className="flex flex-col content-center w-full gap-4 font-light">
+          <div className={`fixed z-40 left-0 top-60 right-0 w-full bg-gray-100 overflow-hidden flex flex-col lg:hidden origin-top duration-700 ${!menuToggle ? "h-0" : "h-full"}`}>
+            <div className="flex flex-col content-center w-full gap-8 font-light">
               <div className="m-auto cursor-pointer hover:underline">Home</div>
               <div className="m-auto cursor-pointer hover:underline">Products</div>
               <div className="m-auto cursor-pointer hover:underline">About</div>
@@ -29,7 +31,7 @@ const Header = () => {
         </div>
         {/* Desktop Version */}
         <div className="hidden lg:flex flex-1 justify-end">
-          <div className="flex flex-row gap-4 w-1/2 justify-evenly mr-10 font-light">
+          <div className="flex flex-row gap-8 mb-8 justify-evenly font-light">
             <div className="m-auto cursor-pointer hover:underline">Home</div>
             <div className="m-auto cursor-pointer hover:underline">Products</div>
             <div className="m-auto cursor-pointer hover:underline">About</div>
