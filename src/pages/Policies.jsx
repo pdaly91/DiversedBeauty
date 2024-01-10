eb2fe9d37a44166731c2e2c1510084093fb9893a
pdaly91 <patrickdaly91@gmail.com>
2024-01-10 10:14:07 -0800

    add temporart styling to policies page

--- src/pages/Policies.jsx
+++ src/pages/Policies.jsx
@@ -3,13 +3,13 @@ import Footer from '../components/Footer.jsx';
 
 const Policies = () => {
   return (
-    <>
+    <div className="h-screen bg-main-light text-main-black">
       <Header />
-      <div>
+      <div className="h-96 mb-auto">
         This is the POLICIES page
       </div>
       <Footer />
-    </>
+    </div>
   )
 };
 
