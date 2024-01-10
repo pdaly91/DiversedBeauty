a3b0e2ff679c023e1a873ef65e056af917ac7f29
pdaly91 <patrickdaly91@gmail.com>
2024-01-09 16:58:44 -0800

    add theme colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main-light': '#ebe1d7',
+        'main-middle': '#C1914D',
+        'main-dark': '#6E4B2B',
+        'main-black': '#120B08',
+      },
+    },
   },
   plugins: [],
 };
