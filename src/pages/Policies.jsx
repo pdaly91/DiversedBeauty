0ca65e8c96f1c1d539a51228cb7faaf4dbc42ed1
pdaly91 <patrickdaly91@gmail.com>
2024-01-10 16:37:20 -0800

    added content to the policies page

--- src/pages/Policies.jsx
+++ src/pages/Policies.jsx
@@ -3,10 +3,98 @@ import Footer from '../components/Footer.jsx';
 
 const Policies = () => {
   return (
-    <div className="h-screen bg-main-light text-main-black">
+    <div className="bg-main-light text-main-black">
       <Header />
-      <div className="h-96 mb-auto">
-        This is the POLICIES page
+      <div className="w-10/12 mx-auto my-8">
+        <h1 className="text-3xl font-light text-center">
+          Return and Refund Policies
+        </h1>
+        <section className="my-12">
+          <h2 className="text-xl font-bold">
+            Business Rights:
+          </h2>
+          <p className="my-4">
+            Ingredients are listed within every product description. Diversed Beauty LLC is not responsible for any allergies or injuries.
+          </p>
+        </section>
+        <section className="my-12">
+          <h2 className="text-xl font-bold">
+            Shipping Policy:
+          </h2>
+          <p className="my-4">
+            Diversed Beauty LLC processing window is 7-10 business days. Order confirmation will be sent upon ordering your products, and a shipping confirmation will be sent via email once US postage receives your order.
+          </p>
+        </section>
+        <section className="my-12">
+          <h2 className="text-xl font-bold">
+            All Sales are Final:
+          </h2>
+          <p className="my-4">
+            At Diversed Beauty, we take pride in delivering high-quality products to our valued customers. To maintain the integrity and hygiene of our cosmetics, all sales are considered final. We do not accept returns or exchanges for reasons of hygiene, including but not limited to lip glosses. We recommend reviewing your order carefully before making a purchase. If you have any questions or concerns, please feel free to contact our customer support team.
+          </p>
+        </section>
+        <section className="my-12">
+          <h2 className="text-xl font-bold">
+            Lost, Stolen, or Damaged Packages:
+          </h2>
+          <p className="my-4">
+            Diversed Beauty LLC is committed to ensuring the safe and timely delivery of your orders. Once your order leaves our facility, it is entrusted to the care of the shipping carrier. We are not responsible for any packages that may be lost, stolen, or damaged during transit. However, we understand that such situations can be frustrating.
+          </p>
+          <p className="my-4">
+            In the event of a lost, stolen, or damaged package, we recommend the following steps:
+          </p>
+          <section className="my-4 mx-8">
+            <p className="my-4">
+              <b>Contact the Carrier: </b>If your tracking information shows that your package has been lost, stolen, or damaged, please contact the shipping carrier directly. They may be able to provide guidance and assistance in resolving the issue.
+            </p>
+            <p className="my-4">
+              <b>Notify Us: </b>We value our customers&apos; satisfaction. While we cannot be responsible for lost, stolen, or damaged packages, we encourage you to contact our customer support team to inform us of the situation. We will do our best to assist you in addressing the issue.
+            </p>
+            <p className="my-4">
+              <b>File a Claim: </b>Depending on the circumstances and the policies of the shipping carrier, you may need to file a claim with the carrier to seek compensation or resolution for the lost, stolen, or damaged package.
+            </p>
+            <p className="my-4">
+              Please note that the outcome of claims with the shipping carrier is subject to their policies and procedures. Diversed Beauty LLC will offer support and guidance to the best of our abilities but is not liable for the final resolution of these cases.
+            </p>
+          </section>
+        </section>
+        <section className="my-12">
+          <h2 className="text-xl font-bold">
+            Address Mistakes and Returns
+          </h2>
+          <p className="my-4">
+            We understand that address mistakes can happen, and sometimes packages may be returned to Diversed Beauty due to errors in the provided address information. In such cases, we want to work with our valued customers to find a fair and reasonable solution.
+          </p>
+          <p className="my-4">
+            If a package is returned to us due to an address error on the customer&apos;s part, we will assess the situation on a case-by-case basis. Depending on the circumstances, we may offer a partial refund of the purchase price of the items in the returned package. Please note that any shipping and handling fees associated with the initial order will not be refunded.
+          </p>
+          <p className="my-4">
+            To avoid address-related issues, we strongly advise our customers to double-check and verify their shipping information before completing a purchase. It&apos;s essential to provide accurate and up-to-date address details to ensure the successful delivery of your order.
+          </p>
+          <p className="my-4">
+            We are committed to delivering our products to you promptly and securely. If you believe that a returned package is the result of an address mistake, please contact our customer support team as soon as possible. We will do our best to assist you in resolving the situation and ensuring you receive your Diversed Beauty products.
+          </p>
+        </section>
+        <section className="my-12">
+          <h2 className="text-xl font-bold">
+            Handmade with Love: Product Variations
+          </h2>
+          <p className="my-4">
+            At Diversed Beauty, we take great pride in the craftsmanship of our products. Each item is handcrafted with love, care, and attention to detail. We believe that the human touch adds a unique and personal quality to our cosmetics.
+          </p>
+          <p className="my-4">
+            Due to the handmade nature of our products, it&apos;s important to note that variations in color, texture, and appearance may occur from one item to another. And due to the nature of our natural ingredients, there may appear to be separation with glosses that contain shimmer but fear not! The quality and fabulousness are not affected! These slight differences are a testament to the individuality and uniqueness of each product, making it one-of-a-kind.
+          </p>
+          <p className="my-4">
+            We believe that these variations add character and charm to our offerings, and we hope you appreciate the beauty of handmade products. As a result, we kindly ask our customers to embrace and celebrate these natural variations as part of the Diversed Beauty experience.
+          </p>
+          <p className="my-4">
+            If you have specific concerns or questions about a product you&apos;ve received, please don&apos;t hesitate to reach out to our customer support team. We are here to assist and ensure your satisfaction with your purchase.
+          </p>
+          <p className="my-4">
+            We appreciate your understanding and support as we work to provide you with the best cosmetics and service. If you have any questions or require further assistance, please don&apos;t hesitate to reach out to our customer support team.
+          </p>
+        </section>
       </div>
       <Footer />
     </div>
